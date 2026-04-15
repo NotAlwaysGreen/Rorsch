8dc57119a75fa30b89553e42acfb4fde15e1b212
NotAlwaysGreen <neuvek.zelen@gmail.com>
2026-04-15 14:45:25 +0200

    shooting works, targets in place

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,16 +1,38 @@
+using System.Reflection;
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifetime = 3f;
+    
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+
+    }
+
+    private void FireWeapon()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position,Quaternion.identity);
         
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet,bulletPrefabLifetime));
+
+
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float bulletPrefabLifetime)
+    {
+        yield return new WaitForSeconds(bulletPrefabLifetime);
+        Destroy(bullet);
     }
 }
