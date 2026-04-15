eff10c6000b089b4285d17d8520aeb382744fffe
NotAlwaysGreen <neuvek.zelen@gmail.com>
2026-04-15 09:18:51 +0200

    GUNFINGERS 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,66 @@ using UnityEngine;
 
 public class PlayerMovementm : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+
+    private bool isGrounded;
+    private bool isMoving;
+
+    private Vector3 lastPosition;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Movement input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        // Movement check
+        if (transform.position != lastPosition && isGrounded)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        lastPosition = transform.position;
     }
 }
